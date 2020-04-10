0a2ce41b9411493dbafceaf8119e188bd94cb992
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2020-04-10 16:48:08 -0300

    Added i2c treatment with yarpie

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from PyQt5 import QtWidgets
 from view.main_window import Ui_RaspDigitalOscilloscope
+from models.I2CTreatment import I2CTreatment
 import sys
+import time
 
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -11,10 +13,19 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
 
 def main():
+    i2c = I2CTreatment()
     app = QtWidgets.QApplication(sys.argv)
     application = ApplicationWindow()
     application.show()
     sys.exit(app.exec_())
+    print("Test")
+    while True:
+        i2c.write(0x51)
+        time.sleep(0.7)
+        lightlvl = i2c.lightlevel()
+        rng = i2c.range()
+        print(lightlvl)
+        print(rng)
 
 if __name__ == "__main__":
     main()
---
+++ src/models/I2CTreatment.py
@@ -0,0 +1,30 @@
+import RPi_emu.GPIO as GPIO
+import time
+import random #random is used to provide a random number as light level
+
+class I2CTreatment:
+    __smbus = None
+    __address = 0x70
+
+    def __init__(self):
+        self.__address = 0x70
+        self.__smbus = GPIO.SMBus(1)
+        random.seed()
+        GPIO.add_autoreply(self.__address, 2, 5)
+        GPIO.add_autoreply(self.__address, 3, 1)
+
+    def write(self, value):
+        self.__smbus.write_byte_data(self.__address, 0, value)
+        #provide a random number to register 1 (light level)
+        GPIO.add_autoreply(self.__address, 1, random.randrange(0, 255, 1))
+        return -1
+
+    def lightlevel(self):
+        light = self.__smbus.read_byte_data(self.__address, 1)
+        return light
+
+    def range(self):
+        range_1 = self.__smbus.read_byte_data(self.__address, 2)
+        range_2 = self.__smbus.read_byte_data(self.__address, 3)
+        range_3 = (range_1 << 8) + range_2
+        return range_3
